2370cfdf18f6f183d763f0bf02247e956a40c771
dtc2903 <duongtancuong292003@gmail.com>
2026-07-07 01:16:00 +0700

    Tao trang them moi lien he

--- src/router/index.js
+++ src/router/index.js
@@ -15,6 +15,11 @@ const routes = [
     component: () => import("@/views/ContactEdit.vue"),
     props: true,
   },
+  {
+    path: "/contacts/add",
+    name: "contact.add",
+    component: () => import("@/views/ContactAdd.vue"),
+  },
 
   {
     path: "/:pathMatch(.*)*",
